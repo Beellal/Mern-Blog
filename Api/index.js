64761e79bdb2cee7aa3321b1baf361b704269eb5
Beellal <mahamanehabiboubilal999@gmail.com>
2024-04-13 23:09:36 +0100

    Create a data base

--- Api/index.js
+++ Api/index.js
@@ -1,7 +1,18 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+//Connecter la base de donner mangodb
+mongoose.connect(process.env.MONGO)
+    .then(
+        () => { console.log('MongoDb is connected !') }
+    )
+    .catch((err) => console.error(err));
+
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Seerveur is running on 3000');
+    console.log('Serveur is running on 3000');
 })
